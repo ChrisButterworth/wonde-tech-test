e539af017d105f0459d5665ebaf1e90a4ca5e3fe
Chris Butterworth <chris@code-designs.co.uk>
2024-05-16 14:13:07 +0100

    Comments updates

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 Uses Laravel and Tailwind
 
-Doesn't use PHP client as it seemed overkill for two calls to the API
+Doesn't use PHP client as it seemed overkill for two calls to the API -  not sure whether this is the right way or not but does the job.
 
 ## TODO
 
  - [ ] Date comparison
  - [ ] Draw calendar for week
- - [ ] Publish to Git
--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -16,7 +16,7 @@ class LessonsController extends Controller
      * show
      * - displays calendar for current week
      * 
-     * NB: including employeed
+     * NB: including employee in API call to be able to filter
      */
 
 
@@ -24,30 +24,31 @@ class LessonsController extends Controller
      * Pulls employess from API to display
      * 
      * Doesn't use client for cleanliness
-     * no params
+     * @param none
+     * @return View
      */
     public function index() {
-        // $response = Http::withToken(env('ACCESS_TOKEN'))->get('https://api.wonde.com/v1.0/schools/'.env('SCHOOL_ID').'/lessons?include=employee,employees,class')->json();
         $response = Http::withToken(env('ACCESS_TOKEN'))->get('https://api.wonde.com/v1.0/schools/'.env('SCHOOL_ID').'/employees')->json();
-
-        // dd($response['data']);
         
         return view('index')->with('employees', $response['data']);
     }
 
+    /**
+     * Request and display lessons based on employee ID
+     * 
+     * @param Request $request - Laravel request object
+     * @param string $id - Employee ID
+     */
     public function show(Request $request, $id) {
         $response = Http::withToken(env('ACCESS_TOKEN'))->get('https://api.wonde.com/v1.0/schools/'.env('SCHOOL_ID').'/lessons?include=employee,employees,class')->json();
         $response = $response['data'];
-        // dd($response);
-        $lessons = [];
         
+        $lessons = [];
         foreach ($response as $lesson) {
             if (isset($lesson['employee']['data']) && $lesson['employee']['data']['id'] == $id) {
                 array_push($lessons, $lesson);
             }
         }
-
-        // dd($lessons);
 
         return view('lessons')->with('lessons', $lessons)->with('employee_id', $id);
     }
